f93cac196cc77f635a7e652658abed7e1a1278fc
liu <2452791011@qq.com>
2023-04-18 09:29:27 +0800

    first commit

---
+++ code-test/src/main/java/com/liu/demo/controller/DemoController.java
@@ -0,0 +1,15 @@
+package com.liu.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/system")
+public class DemoController {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hello World";
+    }
+}
