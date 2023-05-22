8bb9b221d2e95131e73d00e78893c6059f8651a1
liu <2452791011@qq.com>
2023-05-22 14:54:17 +0800

    fix: change port

--- coder_ui/vue.config.js
+++ coder_ui/vue.config.js
@@ -1,4 +1,11 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+
+  // devServer 相关配置
+  devServer: {
+    port: 8848, // 指定端口号，默认是8080
+    open: false, // 是否自动在浏览器中打开页面
+  }
+
 })
